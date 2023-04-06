8804887f3c23cebed00c0ccec0d9e6cad9385974
jaideepsarkar19 <jaideep.sarkar19@rediffmail.com>
2023-04-07 00:11:45 +0530

    1st

--- index.js
+++ index.js
@@ -1,18 +1,17 @@
 import express from "express";
-import { MongoClient } from "mongodb";
+//import { MongoClient } from "mongodb";
 import * as dotenv from "dotenv";
 import mongoose from "mongoose"
 import student from './models/student.js'
 import mentor from "./models/mentor.js"
+import bodyParser from 'body-parser'
 const app = express();
 dotenv.config();
-
 app.use(express.json());
 const PORT = process.env.PORT;
+app.use(bodyParser.urlencoded({ extended: false }))
 //interceptor /converting body to json
-
 // const MONGO_URL = process.env.MONGO_URL;
-
 //Mongo connection
 // async function createConnection() {
 //   const client = new MongoClient(MONGO_URL);
@@ -32,37 +31,59 @@ const connect = async () => {
 // export const client = await createConnection();
 // req => what we send to Server
 // res => what we receive from server
-
 app.get("/", function (req, res) {
-  res.send("Welcome to Student Mnetor Backend");
+  res.send("<h1>Welcome to Student Mentor Backend<h1>");
 });
-app.post("/createstudent",async function (req,res){
-  const {studentname,contactno,mentorassigned}=req.body
-  const newstudent= new student({studentname: studentname,
-  contactno: contactno,
-mentorassigned: mentorassigned })
-     await newstudent.save()
-  res.status(200).send( newstudent)
+app.post("/createstudent", async function (req, res) {
+  const { studentname, contactno, mentorassigned } = req.body
+  const newstudent = new student({
+    studentname: studentname,
+    contactno: contactno,
+    mentorassigned: mentorassigned
+  })
+  await newstudent.save()
+  res.status(200).send(newstudent)
 });
-app.post("/creatementor",async function (req,res){
-const { mentorname,contactno,rating,courses,language,studentenrolled,studentunenrolled}=req.body;
-  const newmentor= new mentor({ mentorname: mentorname,
-  contactno: contactno,
-  rating: rating,
-  courses: courses,
-  language: language,
-  studentenrolled:studentenrolled,
-  studentunenrolled:studentunenrolled
+app.post("/creatementor", async function (req, res) {
+  const { mentorname, contactno, rating, courses, language, studentenrolled, studentunenrolled } = req.body;
+  const newmentor = new mentor({
+    mentorname: mentorname,
+    contactno: contactno,
+    rating: rating,
+    courses: courses,
+    language: language,
+    studentenrolled: studentenrolled,
+    studentunenrolled: studentunenrolled
   })
   await newmentor.save()
-  res.status(200).send( newmentor)
+  res.status(200).send(newmentor)
 })
+//student assigned to mentor
+app.get("/nomentorassigned", async function (req, res) {
+  const a = await student.find({}, { et: { mentorassigned: 0 } })
+  res.send(a)
+});
+
+app.put("/assignstudent/:mentorid", async (req, res) => {
+  const mentorid = req.params["mentorid"]
+  const students= req.body
+  for (const sp in students) {
+    console.log(students[sp])
+    const result = await mentor.findOneAndUpdate({ _id: mentorid }, { $push: { studentenrolled: sp } })
+   const abcd = await student.findOneAndUpdate({ _id: sp }, { $push: { mentorassigned: mentorid } })} 
+  
+  res.send(result)
+})
+
+
+
 
 
 
 // app.use("/", booksRouter);
 
 
-app.listen(PORT,()=> {
+app.listen(PORT, () => {
   connect();
-  console.log(`Server is running on ${PORT}`)})
+  console.log(`Server is running on ${PORT}`)
+})
